c2d0215182c4d44f74a1169a8ba873d2f1df3002
Andy Feng <106421777+iAndfe@users.noreply.github.com>
2023-06-27 20:36:30 +1200

    0.0.2

--- src/App.js
+++ src/App.js
@@ -11,24 +11,39 @@ function App() {
             <div className="flex items-center space-x-2">
               <img src={logo} className="h-7 sm:h-8" />
               <div className="text-2xl font-bold text-gray-700">
-                Debian Minimal
+                Debian Download
               </div>
             </div>
             <div className="divide-y divide-gray-200">
               <div className="pt-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <p>
-                  Welcome to the new Debian Minimal installation guide. Here you
-                  can download the latest version of Debian and get support.
+                  Welcome to Debian Download, a simpler way to download Debian.
+                  Whether you are a professional system administrator or an
+                  enthusiastic hobbyist, we have got the options for you.
                 </p>
-                <div className="mt-6">
+                <div className="mt-6 flex space-x-4">
                   <a
-                    href="https://www.debian.org/distrib/"
+                    href="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.0.0-amd64-netinst.iso"
                     target="_blank"
                     rel="noreferrer"
                     className="cursor-pointer text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                   >
-                    Download Debian Minimal
+                    Minimal
                   </a>
+                  <a
+                    href="https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-12.0.0-amd64-gnome.iso"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="cursor-pointer text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg"
+                  >
+                    GNOME Live
+                  </a>
+                </div>
+                <div className="pt-8">
+                  <p>
+                    Debian is a robust, reliable and respected open-source
+                    operating system. Get your copy today!
+                  </p>
                 </div>
               </div>
             </div>
